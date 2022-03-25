1a6bd58f6cb34716c78e251c9eb3a99aef3fc359
Moongomi <kyrdudn2@naver.com>
2022-03-25 22:04:23 +0900

    check sms permissions

--- app/src/main/java/com/example/smsforward/MainActivity.kt
+++ app/src/main/java/com/example/smsforward/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.smsforward
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.RECEIVE_SMS) !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECEIVE_SMS,Manifest.permission.SEND_SMS),111)
+        }
+        else{
+            receiveMsg()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            receiveMsg()
+        }
+    }
+
+    private fun receiveMsg() {
+        TODO("Not yet implemented")
     }
 }
